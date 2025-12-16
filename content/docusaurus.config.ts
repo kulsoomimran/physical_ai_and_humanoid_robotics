a40fc2d91a4f45b36b48c83c6df7ed0cd6fb091f
Kulsoom Imran <kulsoomimran1901@gmail.com>
2025-12-16 23:33:11 +0500

    update url in docusaurus

--- content/docusaurus.config.ts
+++ content/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-robotics-book.example.com',
+  url: 'https://ai-and-humanoid-robotics-book.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
